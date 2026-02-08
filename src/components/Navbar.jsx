e9391f2cf47b5ededa3f48709303bb924b50c28f
saharadoesdev <smsmo@att.net>
2026-02-08 11:36:42 -0600

    make the navbar better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,10 +49,22 @@ function Navbar() {
                     </Link>
                     <Link
                         to="/create"
-                        className="navbar-link navbar-link-cta"
+                        className={`navbar-link ${isActive('/create') ? 'active' : ''}`}
                         onClick={() => setIsOpen(false)}
                     >
-                        + Share
+                        Share
+                    </Link>
+                    <div className="navbar-divider"></div>
+                    <Link
+                        to="/account"
+                        className="navbar-link navbar-account"
+                        onClick={() => setIsOpen(false)}
+                        title="Account"
+                    >
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <circle cx="12" cy="8" r="4"></circle>
+                            <path d="M20 21a8 8 0 1 0-16 0"></path>
+                        </svg>
                     </Link>
                 </div>
             </div>
